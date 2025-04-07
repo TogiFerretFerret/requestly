5490ae5b16dd1c1af64af2405f7ff612b9edc41e
rohanmathur91 <mathurrohan04@gmail.com>
2025-04-07 19:22:50 +0530

    chore: add generic state events

--- app/src/componentsV2/Tabs/components/TabItem.tsx
+++ app/src/componentsV2/Tabs/components/TabItem.tsx
@@ -3,12 +3,20 @@ import { TabState } from "../store/tabStore";
 import { StoreApi } from "zustand";
 import { GenericStateContext } from "hooks/useGenericState";
 import { useTabServiceWithSelector } from "../store/tabServiceStore";
+import {
+  trackTabGenericStateSetPreviewMode,
+  trackTabGenericStateSetSaved,
+  trackTabGenericStateSetTitle,
+} from "../analytics";
 
 export const TabItem: React.FC<React.PropsWithChildren<{ store: StoreApi<TabState> }>> = React.memo((props) => {
   const [incrementVersion, activeTabId] = useTabServiceWithSelector((state) => [
     state.incrementVersion,
     state.activeTabId,
   ]);
+
+  const sourceId = props.store.getState().source.getSourceId();
+  const sourceType = props.store.getState().source.type;
 
   return (
     <GenericStateContext.Provider
@@ -19,16 +27,19 @@ export const TabItem: React.FC<React.PropsWithChildren<{ store: StoreApi<TabStat
         isNewTab: props.store.getState().source.getIsNewTab(),
 
         setTitle: (title: string) => {
+          trackTabGenericStateSetTitle(sourceId, sourceType);
           props.store.getState().setTitle(title);
           incrementVersion();
         },
 
         setPreview: (preview: boolean) => {
+          trackTabGenericStateSetPreviewMode(sourceId, sourceType, preview);
           props.store.getState().setPreview(preview);
           incrementVersion();
         },
 
         setSaved: (saved: boolean) => {
+          trackTabGenericStateSetSaved(sourceId, sourceType, saved);
           props.store.getState().setSaved(saved);
           incrementVersion();
         },
